bbc4901e2a0dfe9580d8eabb27034db908208193
panda <happyproger01@gmail.com>
2018-12-22 18:49:03 +0300

    Promezhutok nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     // navigation
+    let submenu = $('.submenu').addClass('disable');
     let nav_flag = false;
     let chevron_off = $('#chevron-off');
     let chevron_on = $('#chevron-on').addClass('disable');
     $('#nav-item-catalog').click(function () {
         if (nav_flag === false) {
+
             chevron_on.removeClass('disable');
             chevron_off.addClass('disable');
             nav_flag = true
@@ -13,6 +15,5 @@ $(document).ready(function () {
             chevron_off.removeClass('disable');
             nav_flag = false
         }
-
     })
 });
