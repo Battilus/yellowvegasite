d1e0287374301a08f13dd680742c777c0ae3da82
panda <happyproger01@gmail.com>
2018-12-25 15:01:02 +0300

    nav end

--- static/js/script.js
+++ static/js/script.js
@@ -6,14 +6,29 @@ $(document).ready(function () {
     let chevron_on = $('#chevron-on').addClass('disable');
     $('#nav-item-catalog').click(function () {
         if (nav_flag === false) {
-
+            submenu.removeClass('disable');
             chevron_on.removeClass('disable');
             chevron_off.addClass('disable');
             nav_flag = true
         } else {
+            submenu.addClass('disable');
             chevron_on.addClass('disable');
             chevron_off.removeClass('disable');
             nav_flag = false
         }
-    })
+    });
+
+
+    let url = document.URL.split('/')[3];
+    if (url === '' || url === '#'){
+        $('.topmenu a:contains("Главная")').addClass("active-nav-item")
+    }else if(url === 'about'){
+        $('.topmenu a:contains("Обо мне")').addClass("active-nav-item")
+    }else if(url === 'delivery'){
+        $('.topmenu a:contains("Доставка")').addClass("active-nav-item")
+    }else if(url === 'payment'){
+        $('.topmenu a:contains("Способы оплаты")').addClass("active-nav-item")
+    }else if(url === 'catalog'){
+        $('.topmenu a:contains("Каталог")').addClass("active-nav-item")
+    }
 });
